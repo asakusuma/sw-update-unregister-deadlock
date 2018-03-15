f9482e3105811609c986a6f3f5abe6f40de4aced
akusuma <akusuma@linkedin.com>
2018-03-15 14:00:39 -0700

    Repro error message

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
 // Change the version to create a new worker version
-const version = '21.0.0';
+const version = '40.0.0';
 
 function updateAndUnregister() {
   self.registration.update();
   return self.registration.unregister();
 }
+
+self.addEventListener('activate', event => {
+  console.log('activate', version);
+  event.waitUntil(clients.claim());
+});
 
 self.addEventListener('message', (e) => {
   updateAndUnregister();
